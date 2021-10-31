59a63804cbed0a1b81c47e49dd4689e20187ac24
Steve-Dusty <kuantingk2@gmail.com>
2021-10-30 19:20:25 -0700

    deleted useless "forms.py" file

--- video/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Video
-
-class UploadVideoForm(forms.ModelForm):
-    class Meta:
-        model = Video
-        fields = ['title', 'description', 'video_file', 'unlisted']
