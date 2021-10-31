825092273e2126b4cd9cd8c0bb8dad1d8faf63fc
Steve-Dusty <kuantingk2@gmail.com>
2021-10-31 11:39:25 -0700

    Created "unlisted" functionality!

--- video/models.py
+++ video/models.py
@@ -7,6 +7,6 @@ class Video(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True, default="No Description")
     unlisted = models.BooleanField(default=False)
-    
+
     def __str__(self):
         return self.title
