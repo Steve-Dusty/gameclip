ca8b0b691c8cec5120d3be742a681bcb3d16bfb6
Steve-Dusty <kuantingk2@gmail.com>
2021-11-28 21:10:20 -0800

    added production dependencies

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'video'
+    'video',
+
 ]
 
 MIDDLEWARE = [
@@ -102,6 +103,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = str(BASE_DIR.joinpath('static'))
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
